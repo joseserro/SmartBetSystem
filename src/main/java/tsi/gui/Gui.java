b32252ac75eb4c487e9c82bb33dae3ea3c281b93
Sphincz <raimundo.asl@gmail.com>
2015-01-28 01:28:39 +0000

    ~ Pasta de trabalho actualizada.

--- src/main/java/tsi/gui/Gui.java
+++ src/main/java/tsi/gui/Gui.java
@@ -2,14 +2,8 @@ package main.java.tsi.gui;
 
 import main.java.tsi.viewer.StatusBarNovo;
 
-import javax.imageio.ImageIO;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
 //import java.awt.Color;
 //import java.awt.Point;
@@ -34,57 +28,6 @@ public class Gui {
             fa = new FrameApostas(ca);
             fa.start();
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public static void eh(){
-		try{
-		final JFrame botelho = new JFrame();
-		botelho.setSize(198,204);
-		botelho.setLocation((int)scrSize.getWidth(), ((int)scrSize.getHeight())-244);
-
-		BufferedImage myPicture = ImageIO.read(new File(Gui.class.getResource("/main/resources/tsi/img/botelho.png").toURI()));
-		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-
-		botelho.add(picLabel);
-
-		botelho.setUndecorated(true);
-		botelho.setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
-		botelho.setVisible(true);
-
-		Runnable run = new Runnable(){
-			int move = 0;
-			int moveBy = 1;
-			@Override
-			public void run() {
-				Point loc = botelho.getLocation();
-				while(true){
-					try {
-						botelho.setLocation(loc.x-move, loc.y);
-						move+=moveBy;
-						Thread.sleep(2);
-						if(move > 400){
-							moveBy = -1;
-							AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File("ueh.wav").getAbsoluteFile());
-							Clip clip = AudioSystem.getClip();
-							clip.open(audioIn);
-							clip.start();
-							Thread.sleep(1000);
-						}
-						if(move < 0)
-							break;
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-
-				}
-				botelho.dispose();
-			}
-		};
-		new Thread(run).start();
-		} catch (Exception e){
 			e.printStackTrace();
 		}
 	}
